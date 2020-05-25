13c9c5287d4c7153166b4f7807ccb589193dd3ea
Jessica Cheng <cheng.je@husky.neu.edu>
2020-05-25 14:14:41 -0400

    Updates

---
+++ precache-manifest.c70261ea82848e178a43b99591bb7188.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422fa95ccbbafcd14fdf50f5c652a111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "994fb0185d3e54a408bf",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b7dc8bbcf644d9e75056",
+    "url": "/static/css/main.ecff0db8.chunk.css"
+  },
+  {
+    "revision": "994fb0185d3e54a408bf",
+    "url": "/static/js/2.b8be4be6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8be4be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7dc8bbcf644d9e75056",
+    "url": "/static/js/main.01f60528.chunk.js"
+  },
+  {
+    "revision": "b67166582017c4b6af5f",
+    "url": "/static/js/runtime-main.da2c29c0.js"
+  },
+  {
+    "revision": "58c2cb95c17febfb9ba3bfa256e7b7be",
+    "url": "/static/media/LASky.58c2cb95.jpg"
+  }
+]);
